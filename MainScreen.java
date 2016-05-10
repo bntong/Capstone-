b5de576e3907ac61b9d36f7cf703f313b77de19e
bntong <briantong05@gmail.com>
2016-05-10 06:44:33 -0500

    Capstone
    Finished

--- MainScreen.java
+++ MainScreen.java
@@ -7,6 +7,7 @@ import java.io.ObjectOutputStream;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
 
+@SuppressWarnings("unchecked")
 /**
  * Write a description of class Viewer here.
  * 
